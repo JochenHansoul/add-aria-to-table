39f8ab43b37e3e26aa6aca25bf034e7e84fc9339
JochenHansoul <jochen.hansoul@outlook.be>
2024-09-01 14:23:06 +0200

    Add index.js and package.json

---
+++ index.js
@@ -0,0 +1,26 @@
+// Mapping the scope of the TH elements with their corresponding ARIA roles
+const thRoles = Object.freeze(new Map([
+  ["col", "columnheader"],
+  ["row", "rowheader"],
+  ["colgroup", "columngroupheader"],
+  ["rowgroup", "rowgroupheader"],
+  [null, "columnheader"]
+]));
+
+export const addAriaToTable = function (table) {
+  table.setAttribute("role", "table");
+  table.tHead?.setAttribute("role", "rowgroup");
+  table.tFoot?.setAttribute("role", "rowgroup");
+  Array.from(table.tBodies)
+    .forEach(tBodie => tBodie.setAttribute("role", "rowgroup"));
+  Array.from(table.rows).forEach(function (row) {
+    row.setAttribute("role", "row");
+    Array.from(row.cells).forEach(function (cell) {
+      cell.setAttribute("role", (cell.nodeName === "TD") ? "cell" : thRoles.get(cell.scope))
+    });
+  });
+};
+
+export default const addAriaToAllTables = function () {
+  document.querySelectorAll("table").forEach(table => addAriaToTable(table));
+};
